5a878ef9c71b3e6661e487d329301aeec951d9aa
Guido Draheim <guidod@gmx.de>
2006-08-15 05:29:08 +0000

    zzip>disk  (ChangeLog zzip/mmapped.c)

--- zzip/mmapped.c
+++ zzip/mmapped.c
@@ -103,7 +103,7 @@ zzip_disk_mmap(int fd)
     struct stat st;
     if (fstat (fd, &st) || !st.st_size) return 0;
     ___ ZZIP_DISK* disk = zzip_disk_new (); if (! disk) return 0;
-    disk->buffer = _zzip_mmap (& zzip->mapped, fd, 0, st.st_size);
+    disk->buffer = _zzip_mmap (& disk->mapped, fd, 0, st.st_size);
     if (disk->buffer == MAP_FAILED) { free (disk); return 0; }
     disk->endbuf = disk->buffer + st.st_size;
     return disk; ____;
