bbd4142273f7b9d1889bfab7122eff9433d9a1df
Guido Draheim <guidod@gmx.de>
2018-02-05 02:12:41 +0100

    test_65419 should reproduce CVE-2918-6541 but it is ok

--- test/zziptests.py
+++ test/zziptests.py
@@ -3188,6 +3188,8 @@ class ZZipTest(unittest.TestCase):
     file_url = self.url_CVE_2018_16
     download(file_url, filename, tmpdir)
     exe = self.bins("unzzip")
+    run = shell("cd {tmpdir} && ../{exe} -p {filename} ".format(**locals()),
+        returncodes = [0,3])
     run = shell("cd {tmpdir} && ../{exe} {filename} ".format(**locals()),
         returncodes = [0,3])
     self.assertLess(len(run.output), 30)
