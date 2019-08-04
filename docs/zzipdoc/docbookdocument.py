gdraheim/zziplib

commit 5b660a3e6894f6f095e277d41446e764eab9be4a

Merge pull request #72 from jelly/python3

docs/zzipdoc/docbookdocument.py
@@ -26,14 +26,14 @@ class DocbookDocument:
     def get_title(self):
         if self.title: return title
         try:   return self.text[0].get_title()
-        except Exception: pass
+        except Exception as e: pass
         return self.title
     def _xml_doctype(self, rootnode):
         return "<!DOCTYPE "+rootnode+self.docbook_dtd+">"
     def _xml_text(self, xml):
         """ accepts adapter objects with .xml_text() """
         try:   return xml.xml_text()
-        except Exception as e: print("DocbookDocument/text" + str(e)); pass
+        except Exception as e: print "DocbookDocument/text", e; pass
         return str(xml)
     def _fetch_rootnode(self, text):
         fetch = Match(r"^[^<>]*<(\w+)\b")

docs/zzipdoc/commentmarkup.py
@@ -31,7 +31,7 @@ class CommentMarkup:
         comment = self.header.comment
         try:
             comment = self.header.get_otherlines()
-        except Exception:
+        except Exception as e:
             pass
         mode = ""
         text = ""

docs/zzipdoc/functionlisthtmlpage.py
@@ -45,7 +45,7 @@ class FunctionListHtmlPage:
             callspec = entry.head_get_callspec()
             head_text = ("<code><b><function>"+namespec+"</function></b>"
                          +callspec+" : "+prespec+"</code>")
-        except Exception:
+        except Exception as e:
             pass
         try:
             extraline = ""
@@ -58,7 +58,7 @@ class FunctionListHtmlPage:
                              '<em><small>'+filename+'</small></em>'+
                              '</td></table>')
             body_text = extraline + body_text
-        except Exception:
+        except Exception as e:
             pass
         def link(text):
             return (text & Match("<function>(\w*)</function>")

docs/zzipdoc/htmldocument.py
@@ -31,12 +31,12 @@ class HtmlDocument:
     def get_title(self):
         if self.title: return self.title
         try:   return self.text[0].get_title()
-        except Exception: pass
+        except Exception as e: pass
         return self.title
     def _html_meta(self, meta):
         """ accepts adapter objects with .html_meta() """
         try:   return meta.html_meta()
-        except Exception: pass
+        except Exception as e: pass
         return str(meta)
     def _html_style(self, style):
         """ accepts adapter objects with .html_style() and .xml_style() """
@@ -65,7 +65,7 @@ class HtmlDocument:
                 self.navi = fd.read()
                 fd.close()
                 return self.navi
-            except Exception:
+            except Exception as e:
                 pass
         return None
     def html_header(self):

docs/zzipdoc/textfile.py
@@ -17,7 +17,7 @@ class TextFile:
             self.src_text = fd.read()
             fd.close()
             return True
-        except IOError:
+        except IOError as e:
             pass
         return False
     def assert_src_text(self):

commit a25f606e31154d8868d5fb27c1abbfb001fcba7e

zzipdoc: convert use of print statement
Commit 5b660a3 (Merge pull request #72 from jelly/python3, 2019-08-04)
introduced a conversion to use new-style Python exceptions.
The conversion introduced a new print statements, though, breaking
compatibility with Python 3 again.

Convert the statement to use the print function, instead.

docs/zzipdoc/docbookdocument.py
@@ -33,7 +33,7 @@ class DocbookDocument:
     def _xml_text(self, xml):
         """ accepts adapter objects with .xml_text() """
         try:   return xml.xml_text()
-        except Exception as e: print "DocbookDocument/text", e; pass
+        except Exception as e: print("DocbookDocument/text " + e); pass
         return str(xml)
     def _fetch_rootnode(self, text):
         fetch = Match(r"^[^<>]*<(\w+)\b")