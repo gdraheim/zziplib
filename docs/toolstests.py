de0e88ddd9f7b11c5d1f0a0d981c2280782f25fc
Guido Draheim <guidod@gmx.de>
2023-03-12 12:32:20 +0100

    - pep8

--- docs/toolstests.py
+++ docs/toolstests.py
@@ -176,92 +176,92 @@ class md2dbkTests(TestCase):
                              "</listitem>", "</itemizedlist>"])
     def test_1202(self):
         b = md2dbk.blocks("* a\n\n\nb")
-        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n", 
-                             "</listitem>", "</itemizedlist>", 
+        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
+                             "</listitem>", "</itemizedlist>",
                              "b\n"])
     def test_1203(self):
         b = md2dbk.blocks("* a\n\n\nb\n* c")
-        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n", 
-                             "</listitem>", "</itemizedlist>", "b\n", 
-                             "<itemizedlist>", "<listitem>", "* c\n", 
+        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
+                             "</listitem>", "</itemizedlist>", "b\n",
+                             "<itemizedlist>", "<listitem>", "* c\n",
                              "</listitem>", "</itemizedlist>"])
     def test_1211(self):
         b = md2dbk.blocks("* a\n* x\n\n")
         self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
-                             "</listitem><listitem>", "* x\n", 
+                             "</listitem><listitem>", "* x\n",
                              "</listitem>", "</itemizedlist>"])
     def test_1212(self):
         b = md2dbk.blocks("* a\n* x\n\nb")
         self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
-                             "</listitem><listitem>", "* x\n", 
+                             "</listitem><listitem>", "* x\n",
                              "</listitem>", "</itemizedlist>", "b\n"])
     def test_1213(self):
         b = md2dbk.blocks("* a\n* x\n\nb\n* c")
-        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n", 
+        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
                              "</listitem><listitem>", "* x\n",
                              "</listitem>", "</itemizedlist>", "b\n",
-                             "<itemizedlist>", "<listitem>","* c\n",
+                             "<itemizedlist>", "<listitem>", "* c\n",
                              "</listitem>", "</itemizedlist>"])
     def test_1221(self):
         b = md2dbk.blocks("* a\n** x\n\n")
-        self.assertEqual(b, ["<itemizedlist>", "<listitem>","* a\n", 
-                             "<itemizedlist>", "<listitem>","** x\n", 
-                             "</listitem>", "</itemizedlist>", 
+        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
+                             "<itemizedlist>", "<listitem>", "** x\n",
+                             "</listitem>", "</itemizedlist>",
                              "</listitem>", "</itemizedlist>"])
     def test_1222(self):
         b = md2dbk.blocks("* a\n** x\n\nb")
-        self.assertEqual(b, ["<itemizedlist>", "<listitem>","* a\n", 
-                             "<itemizedlist>", "<listitem>","** x\n", 
-                             "</listitem>", "</itemizedlist>", 
+        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
+                             "<itemizedlist>", "<listitem>", "** x\n",
+                             "</listitem>", "</itemizedlist>",
                              "</listitem>", "</itemizedlist>", "b\n"])
     def test_1223(self):
         b = md2dbk.blocks("* a\n** x\n\nb\n* c")
-        self.assertEqual(b, ["<itemizedlist>", "<listitem>","* a\n", 
-                             "<itemizedlist>", "<listitem>","** x\n", 
-                             "</listitem>", "</itemizedlist>", 
+        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
+                             "<itemizedlist>", "<listitem>", "** x\n",
+                             "</listitem>", "</itemizedlist>",
                              "</listitem>", "</itemizedlist>", "b\n",
-                             "<itemizedlist>", "<listitem>", "* c\n", 
+                             "<itemizedlist>", "<listitem>", "* c\n",
                              "</listitem>", "</itemizedlist>"])
     def test_1224(self):
         b = md2dbk.blocks("* a\n** x\n*** y\nb\n* c")
-        self.assertEqual(b, ["<itemizedlist>", "<listitem>","* a\n", 
-                             "<itemizedlist>", "<listitem>","** x\n", 
-                             "<itemizedlist>", "<listitem>","*** y\n",
-                             "</listitem>", "</itemizedlist>", 
-                             "</listitem>", "</itemizedlist>", 
+        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
+                             "<itemizedlist>", "<listitem>", "** x\n",
+                             "<itemizedlist>", "<listitem>", "*** y\n",
+                             "</listitem>", "</itemizedlist>",
+                             "</listitem>", "</itemizedlist>",
                              "</listitem>", "</itemizedlist>", "b\n",
-                             "<itemizedlist>", "<listitem>", "* c\n", 
+                             "<itemizedlist>", "<listitem>", "* c\n",
                              "</listitem>", "</itemizedlist>"])
     def test_1225(self):
         b = md2dbk.blocks("* a\n** x\n** y\nb\n* c")
-        self.assertEqual(b, ["<itemizedlist>", "<listitem>","* a\n", 
-                             "<itemizedlist>", "<listitem>","** x\n",
-                             "</listitem><listitem>", "** y\n", 
-                             "</listitem>", "</itemizedlist>", 
+        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
+                             "<itemizedlist>", "<listitem>", "** x\n",
+                             "</listitem><listitem>", "** y\n",
+                             "</listitem>", "</itemizedlist>",
                              "</listitem>", "</itemizedlist>", "b\n",
-                             "<itemizedlist>", "<listitem>","* c\n", 
+                             "<itemizedlist>", "<listitem>", "* c\n",
                              "</listitem>", "</itemizedlist>"])
     def test_1226(self):
         b = md2dbk.blocks("* a\n** x\n* y\n\nb")
-        self.assertEqual(b, ["<itemizedlist>", "<listitem>","* a\n", 
-                             "<itemizedlist>", "<listitem>","** x\n", 
-                             "</listitem>", "</itemizedlist>", "<listitem>", "* y\n", 
+        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
+                             "<itemizedlist>", "<listitem>", "** x\n",
+                             "</listitem>", "</itemizedlist>", "<listitem>", "* y\n",
                              "</listitem>", "</itemizedlist>", "b\n"])
     def test_1227(self):
         b = md2dbk.blocks("* a\n** x\n* y\n* z\n\nb")
-        self.assertEqual(b,  ["<itemizedlist>", "<listitem>","* a\n", 
-                             "<itemizedlist>", "<listitem>","** x\n", 
-                             "</listitem>", "</itemizedlist>", 
+        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
+                             "<itemizedlist>", "<listitem>", "** x\n",
+                             "</listitem>", "</itemizedlist>",
                              "<listitem>", "* y\n",
-                             "</listitem><listitem>", "* z\n", 
+                             "</listitem><listitem>", "* z\n",
                              "</listitem>", "</itemizedlist>", "b\n"])
     def test_1228(self):
         b = md2dbk.blocks("* a\n** x\n* y\n** z\n\nb")
-        self.assertEqual(b, ["<itemizedlist>", "<listitem>","* a\n", 
-                             "<itemizedlist>", "<listitem>","** x\n", 
-                             "</listitem>", "</itemizedlist>", "<listitem>", "* y\n", 
-                             "<itemizedlist>", "<listitem>","** z\n", 
-                             "</listitem>", "</itemizedlist>", 
+        self.assertEqual(b, ["<itemizedlist>", "<listitem>", "* a\n",
+                             "<itemizedlist>", "<listitem>", "** x\n",
+                             "</listitem>", "</itemizedlist>", "<listitem>", "* y\n",
+                             "<itemizedlist>", "<listitem>", "** z\n",
+                             "</listitem>", "</itemizedlist>",
                              "</listitem>", "</itemizedlist>", "b\n"])
     def test_2000(self):
         b = md2dbk.blocks("")
@@ -280,23 +280,23 @@ class md2dbkTests(TestCase):
         self.assertEqual(b, ["<blockquote>", "a\nb\n", "</blockquote>"])
     def test_2005(self):
         b = md2dbk.blocks("> a\n> b\n\n> c")
-        self.assertEqual(b, ["<blockquote>", "a\nb\n", "</blockquote>", 
+        self.assertEqual(b, ["<blockquote>", "a\nb\n", "</blockquote>",
                              "<blockquote>", "c\n", "</blockquote>"])
     def test_2006(self):
         b = md2dbk.blocks("> a\n> b\n\n> c\n")
-        self.assertEqual(b, ["<blockquote>", "a\nb\n", "</blockquote>", 
+        self.assertEqual(b, ["<blockquote>", "a\nb\n", "</blockquote>",
                              "<blockquote>", "c\n", "</blockquote>"])
     def test_2007(self):
         b = md2dbk.blocks("> a\n> b\n\n\n> c")
-        self.assertEqual(b, ["<blockquote>", "a\nb\n", "</blockquote>", 
+        self.assertEqual(b, ["<blockquote>", "a\nb\n", "</blockquote>",
                              "<blockquote>", "c\n", "</blockquote>"])
     def test_2008(self):
         b = md2dbk.blocks("> a\n> b\n\n\n> c\n")
-        self.assertEqual(b, ["<blockquote>", "a\nb\n", "</blockquote>", 
+        self.assertEqual(b, ["<blockquote>", "a\nb\n", "</blockquote>",
                              "<blockquote>", "c\n", "</blockquote>"])
     def test_2009(self):
         b = md2dbk.blocks("> a\n> b\n\n\n> c\n\n")
-        self.assertEqual(b, ["<blockquote>", "a\nb\n", "</blockquote>", 
+        self.assertEqual(b, ["<blockquote>", "a\nb\n", "</blockquote>",
                              "<blockquote>", "c\n", "</blockquote>"])
     def test_2101(self):
         b = md2dbk.blocks("> # a")
@@ -318,58 +318,58 @@ class md2dbkTests(TestCase):
         self.assertEqual(b, ["<blockquote>", "## a\n", "</blockquote>"])
     def test_2151(self):
         b = md2dbk.blocks("> # a\n>\n>\n> b")
-        self.assertEqual(b, ["<blockquote>","# a\n", "b\n", "</blockquote>"])
+        self.assertEqual(b, ["<blockquote>", "# a\n", "b\n", "</blockquote>"])
     def test_2152(self):
         b = md2dbk.blocks("> ## a\n>\n> b")
-        self.assertEqual(b, ["<blockquote>","## a\n", "b\n", "</blockquote>"])
+        self.assertEqual(b, ["<blockquote>", "## a\n", "b\n", "</blockquote>"])
     def test_2153(self):
         b = md2dbk.blocks("> ### a\n>\n>\n>\n> b")
-        self.assertEqual(b, ["<blockquote>","### a\n", "b\n", "</blockquote>"])
+        self.assertEqual(b, ["<blockquote>", "### a\n", "b\n", "</blockquote>"])
     def test_2154(self):
         b = md2dbk.blocks("> #### a\n>\n>\n> b")
-        self.assertEqual(b, ["<blockquote>","#### a\n", "b\n", "</blockquote>"])
+        self.assertEqual(b, ["<blockquote>", "#### a\n", "b\n", "</blockquote>"])
     def test_2155(self):
         b = md2dbk.blocks("> ##### a\n>\n>\n> ## b")
-        self.assertEqual(b, ["<blockquote>","##### a\n", "## b\n", "</blockquote>"])
+        self.assertEqual(b, ["<blockquote>", "##### a\n", "## b\n", "</blockquote>"])
     def test_2156(self):
         b = md2dbk.blocks("> ###### a\n>\n>\n> ## b")
-        self.assertEqual(b, ["<blockquote>","###### a\n", "## b\n", "</blockquote>"])
+        self.assertEqual(b, ["<blockquote>", "###### a\n", "## b\n", "</blockquote>"])
 
 if __name__ == "__main__":
     # main()
-  import optparse
-  _o = optparse.OptionParser("%prog [options] test_xxx")
-  _o.add_option("--xmlresults", metavar="FILE", default=None,
-    help="capture results as a junit xml file [%default]")
-  _o.add_option("-v", "--verbose", action="count", default=0,
-    help="increase logging output [%default]")
-  opt, args = _o.parse_args()
-  logging.basicConfig(level = logging.WARNING - 10 * opt.verbose)
-  #
-  if not args: args += [ "test_" ]
-  suite = TestSuite()
-  for arg in args:
-    for classname in sorted(list(globals())):
-      if not classname.endswith("Tests"):
-        continue
-      testclass = globals()[classname]
-      for method in sorted(dir(testclass)):
-        if "*" not in arg: arg += "*"
-        if arg.startswith("_"): arg = arg[1:]
-        if matches(method, arg):
-          suite.addTest(testclass(method))
-  xmlresults = opt.xmlresults
-  if xmlresults:
-    try: import xmlrunner
-    except: xmlresults=None
-  if xmlresults:
-    if os.path.exists(opt.xmlresults):
-      os.remove(opt.xmlresults)
-    logg.info("xml results into %s", opt.xmlresults)
-    Runner = xmlrunner.XMLTestRunner
-    result = Runner(output=opt.xmlresults).run(suite)
-  else:
-    Runner = TextTestRunner
-    result = Runner(verbosity=opt.verbose).run(suite)
-  if not result.wasSuccessful():
-    sys.exit(1)
+    import optparse
+    _o = optparse.OptionParser("%prog [options] test_xxx")
+    _o.add_option("--xmlresults", metavar="FILE", default=None,
+                  help="capture results as a junit xml file [%default]")
+    _o.add_option("-v", "--verbose", action="count", default=0,
+                  help="increase logging output [%default]")
+    opt, args = _o.parse_args()
+    logging.basicConfig(level=logging.WARNING - 10 * opt.verbose)
+    #
+    if not args: args += ["test_"]
+    suite = TestSuite()
+    for arg in args:
+        for classname in sorted(list(globals())):
+            if not classname.endswith("Tests"):
+                continue
+            testclass = globals()[classname]
+            for method in sorted(dir(testclass)):
+                if "*" not in arg: arg += "*"
+                if arg.startswith("_"): arg = arg[1:]
+                if matches(method, arg):
+                    suite.addTest(testclass(method))
+    xmlresults = opt.xmlresults
+    if xmlresults:
+        try: import xmlrunner
+        except: xmlresults = None
+    if xmlresults:
+        if os.path.exists(opt.xmlresults):
+            os.remove(opt.xmlresults)
+        logg.info("xml results into %s", opt.xmlresults)
+        Runner = xmlrunner.XMLTestRunner
+        result = Runner(output=opt.xmlresults).run(suite)
+    else:
+        Runner = TextTestRunner
+        result = Runner(verbosity=opt.verbose).run(suite)
+    if not result.wasSuccessful():
+        sys.exit(1)
