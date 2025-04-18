7fbadd6249f8dfbf4d41b03f87ba9c924026e482
Guido Draheim <guidod@gmx.de>
2025-04-18 20:46:46 +0200

    + format

--- zzip/mmapped.c
+++ zzip/mmapped.c
@@ -513,17 +513,17 @@ zzip_disk_findnext(ZZIP_DISK* disk, struct zzip_disk_entry* entry)
         errno = EINVAL;
         return 0;
     }
-    if ((zzip_byte_t*) entry < disk->buffer ||                 /* .. */
+    if ((zzip_byte_t*) entry < disk->buffer ||                  /* .. */
         (zzip_byte_t*) entry > disk->endbuf - sizeof(*entry) || /* .. */
-        ! zzip_disk_entry_check_magic(entry) ||                /* .. */
+        ! zzip_disk_entry_check_magic(entry) ||                 /* .. */
         zzip_disk_entry_sizeto_end(entry) > 64 * 1024) {
         errno = EBADMSG;
         return 0;
     }
     entry = zzip_disk_entry_to_next_entry(entry);
     if ((zzip_byte_t*) entry > disk->endbuf - sizeof(*entry) || /* .. */
-        ! zzip_disk_entry_check_magic(entry) ||                /* .. */
-        zzip_disk_entry_sizeto_end(entry) > 64 * 1024 ||       /* .. */
+        ! zzip_disk_entry_check_magic(entry) ||                 /* .. */
+        zzip_disk_entry_sizeto_end(entry) > 64 * 1024 ||        /* .. */
         zzip_disk_entry_skipto_end(entry) > disk->endbuf - sizeof(*entry)) {
         errno = ENOENT;
         return 0;
